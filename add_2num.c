a2d0e5c66da941b701f08dfe64df60e0bba9140b
Peta Ramu <30334104+ramupeta@users.noreply.github.com>
2021-06-05 15:10:53 +0530

    Update add_2num.c

--- add_2num.c
+++ add_2num.c
@@ -2,9 +2,12 @@
 #include<string.h>
 int main()
 {
+  extern int k;
 int a,b,c;
 printf("Enter a and b value:");
 scanf("%d %d",&a,&b);
 c=a+b;
 printf("the c value is:%d",c);
+printf("%c\n",k);
 }
+int k;
